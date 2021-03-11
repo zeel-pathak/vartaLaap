13a8ce78ff5db658a97546a1943c463f834a2519
Zeel Pathak <zeelpathak2018.it@gmail.com>
2021-03-12 00:13:06 +0530

    Changed the port number

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ io.on('connection', (socket) => {
   //     });
  });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(5000, () => {
+  console.log('listening on *:5000');
 });
